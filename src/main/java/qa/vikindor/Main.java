615b5c0dbae83b6f6ea5530c10465bc612ba0f7f
Vikindor <Vikindor@users.noreply.github.com>
2025-08-05 21:11:04 +0200

    Update Main.java

--- src/main/java/qa/vikindor/Main.java
+++ src/main/java/qa/vikindor/Main.java
@@ -3,7 +3,7 @@ package qa.vikindor;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello people!!!");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
